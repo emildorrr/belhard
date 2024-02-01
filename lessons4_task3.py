f621819dd618e75443576f0d2edb7f7aabf5a196
emildorrr <emil.istomin2015@gmail.com>
2024-02-01 20:13:27 +0300

    commit

--- lessons4_task3.py
+++ lessons4_task3.py
@@ -1,6 +1,10 @@
-new_dict = {}
-for z in range(1):
-    y = z + 1
-new_dict[y] = {"name" : input(), "email" : input()}
+# new_dict = {}
+# for z in range(1):
+#     y = z + 1
+# new_dict[y] = {"name" : input(), "email" : input()}
+#
+# print(new_dict)
 
-print(new_dict)
+
+
+
