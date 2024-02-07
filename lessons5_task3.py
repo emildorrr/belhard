35facefa69921934763a04d0af7d01f3b629acc8
emildorrr <emil.istomin2015@gmail.com>
2024-02-07 16:42:37 +0300

    The task is completed

--- lessons5_task3.py
+++ lessons5_task3.py
@@ -1,6 +1,12 @@
 
 n = int(input())
+num5 = 1
 for i in range(2, n+1):
     if i % 2 != 0:
         continue
-    print(i, end = " ")
+    if num5 != 5:
+        print(i, end = " ")
+        num5 += 1
+    else:
+        print(i, sep = "")
+        num5 = 1
